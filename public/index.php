e7652b1a70191aba4b47f2c5fde6aeb3e863f884
Pedro H. Santiago <pedroh_s_@hotmail.com>
2016-11-01 00:42:15 -0200

    request response json

--- public/index.php
+++ public/index.php
@@ -14,12 +14,14 @@
 
     if($_SESSION['start'] + 10 < time()){
 
-		echo json_encode( array('sessao' => 'expirada' ) );	
+		echo json_encode( array('sessao' => 'expirada', 
+								'your_get' => $_GET ) );	
 		$_SESSION = array();
     	session_destroy();
 
     }else{
 
-		echo json_encode( array('sessao' => $_SESSION['start'] + 10 - time() . 'ms restante' ) );	
+		echo json_encode( array('sessao' => $_SESSION['start'] + 10 - time() . 'ms restante', 
+								'your_get' => $_GET ) );	
 
     }
