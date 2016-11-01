2ae6ca4307cb101e6c3dbacae2a86fe318bca981
Pedro H. Santiago <pedroh_s_@hotmail.com>
2016-11-01 00:10:16 -0200

    session

--- public/index.php
+++ public/index.php
@@ -2,17 +2,18 @@
 	if (session_status() == PHP_SESSION_NONE) {
 	    session_start();
 		$_SESSION['start'] = time(); 
+		$_SESSION['expire'] = $_SESSION['start'] + (2 * 60);
 	}
 
 
-	$_SESSION['expire'] = $_SESSION['start'] + (2 * 60);
 
     if(time() > $_SESSION['expire']){
 
 		echo json_encode( array('sessao' => 'expirada' ) );	
-    
+    	session_destroy();
+
     }else{
 
-		echo json_encode( array('sessao' => $_SESSION['expire'] . 'ms restante' ) );	
+		echo json_encode( array('sessao' => $_SESSION['start'] - $_SESSION['expire'] . 'ms restante' ) );	
 
     }
