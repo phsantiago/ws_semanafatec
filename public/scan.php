253321ca5a517d041fb2e240cb1fedeb33e72b4e
Pedro H. Santiago <pedroh_s_@hotmail.com>
2016-11-04 01:11:32 -0200

    modifications

---
+++ public/scan.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width">
+	</head>
+	<body>
+		<!-- <a href="zxing://scan/?ret=#ret={CODE}">  -->
+		    <button onClick="readBarcode()">My scan TEST</button>
+		<!-- </a> -->
+		<script type="text/javascript">
+			function readBarcode(){
+				
+				var href=window.location.href;//works
+				
+				var ptr=href.lastIndexOf("#");
+				
+				if(ptr>0){
+					href=href.substr(0,ptr);
+				}
+
+				window.location.href="zxing://scan/?ret="+escape(href+"#{CODE}");
+			}
+		</script>
+	</body>
+</html>
